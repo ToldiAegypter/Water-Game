f5016ac0ccf407c943399cd748c28335735d8237
dietsch@qdrei.info <dietsch@qdrei.info>
2018-07-31 10:59:41 +0200

    anfang

---
+++ T-Shirt Minigame/Assets/Scripts/Counter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    int Gesamtpunkte = 0;
+    int heart = 3;
+    public TextMesh Countertext;
+    private GameObject herz;
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+
+    public void MehrPunkte(int Punkte)
+    {
+        Gesamtpunkte = Gesamtpunkte + Punkte;
+        Countertext.text = Gesamtpunkte.ToString();
+    }
+     public void Hearts(int heartSub)
+    {
+        if (heart >= 2) {
+            heart = heart - heartSub;
+                }
+        else {
+            herz = GameObject.Find("Herz1");
+            GameObject.Destroy(herz);
+            //goto "Game Over"
+            UnityEngine.SceneManagement.SceneManager.LoadScene("DieScene");
+        }
+        if(heart == 2)
+        {
+            herz = GameObject.Find("Herz2");
+            GameObject.Destroy(herz);
+        }
+        if (heart == 1)
+        {
+            herz = GameObject.Find("Herz3");
+            GameObject.Destroy(herz);
+        }
+    }
+}
---
+++ T-Shirt Minigame/Assets/Scripts/Spawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+	 public Rigidbody2D NormalesTShirt;
+     public Rigidbody2D AltesTShirt;  
+
+	 // Use this for initialization
+	 void Start ()
+	 {
+        nextActionTime = Time.time;
+
+     }
+
+	 void spawn ()
+	 {
+        if (Random.Range(0f, 1f) > 0.5f)
+        {
+            Instantiate(AltesTShirt, transform.position, Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward));
+        }
+        else
+        {
+            Instantiate(NormalesTShirt, transform.position, Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward));
+        }
+	 }
+
+	 private float nextActionTime = 0.0f;
+	 public float secondsBetweenSpawn = 3f;
+
+	 void Update ()
+	 {
+		  if ( Time.time > nextActionTime ) {
+				nextActionTime += secondsBetweenSpawn;
+
+				spawn();
+		  }
+	 }
+
+}
